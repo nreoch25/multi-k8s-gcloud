3a626a6454d33dc125f249ea4f1578b865148cf0
Nigel Reoch <nigel.reoch@telus.com>
2019-02-16 22:21:37 -0500

    fixed ingress

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ docker push nreoch9/multi-worker:latest
 docker push nreoch9/multi-client:$SHA
 docker push nreoch9/multi-server:$SHA
 docker push nreoch9/multi-worker:$SHA
-kubectl apply -f k8s
+kubectl apply -f k8s/
 kubectl set image deployments/client-deployment client=nreoch9/multi-client:$SHA
 kubectl set image deployments/server-deployment server=nreoch9/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=nreoch9/multi-worker:$SHA
