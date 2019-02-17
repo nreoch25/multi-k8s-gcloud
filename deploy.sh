ada4e59b1dba27c114236f7d9dce340300dc3b67
Nigel Reoch <nigel.reoch@telus.com>
2019-02-16 21:42:08 -0500

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t nreoch9/multi-client:latest -t nreoch9/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nreoch9/multi-server:latest -t nreoch9/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nreoch9/multi-worker:latest -t nreoch9/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push nreoch9/multi-client:latest
+docker push nreoch9/multi-server:latest
+docker push nreoch9/multi-worker:latest
+docker push nreoch9/multi-client:$SHA
+docker push nreoch9/multi-server:$SHA
+docker push nreoch9/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=nreoch9/multi-client:$SHA
+kubectl set image deployments/server-deployment server=nreoch9/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=nreoch9/multi-worker:$SHA
